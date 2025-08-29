f771683fa7d1311bcdf545c8939f2b79341e670a
APorkolab <adam.porkolab@gmail.com>
2025-08-29 11:27:15 +0200

    chore: initial project structure with frontend & functions

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  template: `
+    <h1>🌱 Welcome to Cziczere</h1>
+    <p>Your digital memory garden is growing...</p>
+  `
+})
+export class AppComponent {}
